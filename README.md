7f3e38ae2ad366c70d309329a5f6d687076b16d8
phoenixMag00 <rees.will@gmail.com>
2014-05-15 14:21:47 -0400

    First Upload

--- README.md
+++ README.md
@@ -2,3 +2,8 @@ JSON-Sitemap-Generator-for-Grunt-UnCSS-with-WordPress
 =====================================================
 
 JSON Sitemap Generator for Grunt-UnCSS with WordPress
+
+Get a complete JSON Sitemap for use with the Grunt-UnCSS with Wordpress tutorial by Liam Gladdy (http://www.gladdy.co.uk/). Forked off
+his original gist
+
+Upload to your plugins folder and activate. Then go to: http://yourdomain.com?show_sitemap
---
+++ grunt-plugin.php
@@ -0,0 +1,92 @@
+<?php
+/**
+ * Plugin Name: Grunt Sitemap Generator
+ * Plugin URI: http://www.github.com/lgladdy
+ * Description: Generate a JSON list of every page on a site so it can be used with grunt and uncss. Create a folder in /wp-content called mu-plugins, and drop this code into that folder, as grunt-sitemap.php
+ * Author: Liam Gladdy /\ Will Rees
+ * Author URI: http://gladdy.co.uk
+ * Version: 1.1
+ */
+ 
+add_action('template_redirect','show_sitemap');
+
+function show_sitemap() {
+  if (isset($_GET['show_sitemap'])) {
+    $the_query = new WP_Query(array('post_type' => 'any', 'posts_per_page' => '-1', 'post_status' => 'publish'));
+    $urls = array();
+    while ($the_query->have_posts()) {
+      $the_query->the_post();
+      $urls[] = get_permalink();
+      
+    }
+
+	//Homepage
+	
+		$urls[] = get_site_url();
+		
+	//Authors
+
+		 $authors = get_users();
+		 
+		 foreach ($authors as $author) {
+		
+			 $urls[] = get_author_posts_url( $author->ID );
+			 
+		 }
+		  
+	 //Every term imaginable, even the empty ones (categories, custom taxonomies, tags, etc.)
+	  
+		 $taxonomies=get_taxonomies('','names'); 
+		
+		 	$args = array( 'hide_empty=0' );
+		
+		 		$terms = get_terms($taxonomies, $args);
+		
+				    foreach ($terms as $term) {
+				     
+				    	$urls[] = get_term_link( $term );    	
+				    }
+				    
+	//Getting a list of Archive URLs seems like it should be easier...probably missing something on the Codex, but this works.			    	    
+				    
+		$args = array(
+			'type'            => 'monthly',
+			'format'          => 'custom', 
+			'before'          => '',
+			'after'           => '',
+			'show_post_count' => false,
+			'echo'            => 0
+		); 
+					    
+			$archive_links_raw = wp_get_archives($args);
+		
+			$archive_links_pattern = "/(?i)\b((?:https?:\/\/|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}\/)(?:[^\s()<>]+|\(([^\s()<>]+|(\([^\s()<>]+\)))*\))+(?:\(([^\s()<>]+|(\([^\s()<>]+\)))*\)|[^\s`!()\[\]{};:'\".,<>?«»“”‘’]))/";	
+				
+				preg_match_all($archive_links_pattern, $archive_links_raw, $cleaned_archive_links);
+					
+					$cleaned_archive_links = $cleaned_archive_links[1];
+					
+						foreach ($cleaned_archive_links as $cleaned_archive_link) {
+						
+							$urls[] = $cleaned_archive_link;
+						
+						}
+						
+	//Add in a search result page for '.' 
+	
+		$main_url = get_site_url();
+		
+			$urls[] = $main_url . '/?s=.';
+			
+	//Force a 404 page
+	
+		$urls[] = $main_url . '/asdfasdfasdfasdf';
+
+//Return all of the urls captured above and encode to json for UnCSS
+
+	die(json_encode($urls));
+  
+  }
+
+}
+?>
