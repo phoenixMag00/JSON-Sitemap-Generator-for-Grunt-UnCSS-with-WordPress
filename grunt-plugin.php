09d2a8721579dd925cb37cd64d0dc81030f0ee51
phoenixMag00 <rees.will@gmail.com>
2014-05-15 16:04:46 -0400

    Rearranged and format matching
    nothing exciting at all

--- grunt-plugin.php
+++ grunt-plugin.php
@@ -12,13 +12,22 @@ add_action('template_redirect','show_sitemap');
 
 function show_sitemap() {
   if (isset($_GET['show_sitemap'])) {
-    $the_query = new WP_Query(array('post_type' => 'any', 'posts_per_page' => '-1', 'post_status' => 'publish'));
-    $urls = array();
-    while ($the_query->have_posts()) {
-      $the_query->the_post();
-      $urls[] = get_permalink();
-      
-    }
+    
+    //set up an array for all the URLs
+    
+    	$urls = array();
+	
+	//get all the pages, posts (including CPTs)
+	
+		$the_query = new WP_Query(array('post_type' => 'any', 'posts_per_page' => '-1', 'post_status' => 'publish'));
+    
+		while ($the_query->have_posts()) {
+		 
+		  $the_query->the_post();
+		  
+		  	$urls[] = get_permalink();
+	  
+		}
 
 	//Homepage
 	
